08bfdf42622455d317835bab873d65d99a87d6f6
Larissa Rodrigues <larissarodrigues1599@gmail.com>
2025-11-12 15:33:00 -0300

    Start projeto cupom

---
+++ src/main/java/com/example/cupom/CupomApplication.java
@@ -0,0 +1,16 @@
+package com.example.cupom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupomApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CupomApplication.class, args);
+
+	}
+
+
+}

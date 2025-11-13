b2d10d947fd612650417d3e3e8899b8acd1268cd
Larissa Rodrigues <larissarodrigues1599@gmail.com>
2025-11-13 01:20:57 -0300

    feat: src/main/java/exceptions/EntityNotFoundException.java

--- src/main/java/exceptions/EntityNotFoundException.java
+++ src/main/java/exceptions/EntityNotFoundException.java
@@ -1,4 +1,7 @@
 package exceptions;
 
-public class EntityNotFoundException {
+public class EntityNotFoundException extends RuntimeException{
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
 }
